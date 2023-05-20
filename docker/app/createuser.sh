043f2113b53940e601c1fbd6f4070fd0f6f71100
yakibuta888 <yakibuta888@gmail.com>
2023-05-21 03:39:23 +0900

    devcontainer.json内で環境変数(.env)を利用する試み（失敗）

--- docker/app/createuser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-USERNAME=${USER_NAME}
-USERID=${USER_ID}
-GROUPID=${GROUP_ID}
-echo "Create User = $USERID. Group = $GROUPID"
-groupadd -g $GROUPID $USERNAME
-useradd -m -s /bin/bash -u $USERID -g $GROUPID $USERNAME
-exec /usr/sbin/gosu $USERNAME "$@"
